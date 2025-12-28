9ea51f6edf0f4ea16078b154c640149a00e43ae9
Inyte13 <luisgardaz13@gmail.com>
2025-12-27 22:49:30 -0500

    database para mysql

---
+++ backend/app/core/database.py
@@ -0,0 +1,20 @@
+from typing import Annotated
+
+from fastapi import Depends
+from sqlmodel import Session, SQLModel, create_engine
+
+DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/runtime_db"
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+
+def create_db_and_tables():
+  SQLModel.metadata.create_all(engine)  # Crear nuestra tablas
+
+
+def get_session():
+  with Session(engine) as session:  # Generando una nueva session para cada conexión
+    yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
