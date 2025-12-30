035427bf3e0b30180b59f5c5e56e65457e0787e0
Inyte13 <luisgardaz13@gmail.com>
2025-12-29 22:20:06 -0500

    fin bloque service

--- backend/app/services/bloque_service.py
+++ backend/app/services/bloque_service.py
@@ -3,18 +3,15 @@ from datetime import date, time
 from fastapi import HTTPException, status
 from sqlmodel import Session, desc, select
 
-from app.crud.bloque_crud import read_bloque_by_id
+from app.crud.bloque_crud import (
+  create_bloque,
+  delete_bloque,
+  read_bloque_by_id,
+  update_bloque,
+)
 from app.models.actividad import Actividad
 from app.models.bloque import Bloque
-
-
-def buscar_bloque(session: Session, id: int) -> Bloque:
-  bloque = read_bloque_by_id(session, id)
-  if not bloque:
-    raise HTTPException(
-      status_code=status.HTTP_404_NOT_FOUND, detail="Bloque no encontrado"
-    )
-  return bloque
+from app.schemas.bloque_schema import BloqueCreate, BloqueUpdate
 
 
 def _validar_actividad(session: Session, id: int) -> None:
@@ -49,4 +46,41 @@ def _validar_hora_superior(session: Session, fecha: date, hora: time) -> None:
       status_code=status.HTTP_400_BAD_REQUEST,
       detail=f"La hora debe ser posterior a {last_bloque.hora.strftime('%H:%M')}",
     )
+  return
+
+
+def buscar_bloque(session: Session, id: int) -> Bloque:
+  bloque = read_bloque_by_id(session, id)
+  if not bloque:
+    raise HTTPException(
+      status_code=status.HTTP_404_NOT_FOUND, detail="Bloque no encontrado"
+    )
+  return bloque
+
+
+def registrar_bloque(session: Session, bloque: BloqueCreate) -> Bloque:
+  _validar_actividad(session, bloque.id_actividad)
+  _validar_hora_granulidad(bloque.hora, unidad_bloque=30)
+  fecha_actual = date.today()
+  _validar_hora_superior(session, fecha_actual, bloque.hora)
+  new_bloque = Bloque.model_validate({**bloque.model_dump(), "fecha": fecha_actual})
+  return create_bloque(session, new_bloque)
+
+
+def actualizar_bloque(session: Session, id: int, bloque: BloqueUpdate) -> Bloque:
+  bloque_bd = buscar_bloque(session, id)
+  # Si la actividad se ingresó y es diferente a la actual
+  if bloque.id_actividad and bloque.id_actividad != bloque_bd.id_actividad:
+    # Validamos la actividad ingresada
+    _validar_actividad(session, bloque.id_actividad)
+  # Si la hora se ingresó
+  if bloque.hora:
+    _validar_hora_granulidad(bloque.hora, unidad_bloque=30)
+    _validar_hora_superior(session, bloque_bd.fecha, bloque.hora)
+  return update_bloque(session, bloque_bd, bloque)
+
+
+def eliminar_bloque(session: Session, id: int) -> None:
+  bloque = buscar_bloque(session, id)
+  delete_bloque(session, bloque)
   return
