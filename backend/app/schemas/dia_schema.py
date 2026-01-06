dcf235cc0d20357495a8877af8f20bd8fd9c5f5b
Inyte13 <luisgardaz13@gmail.com>
2026-01-06 16:35:31 -0500

    crear dia manual

--- backend/app/schemas/dia_schema.py
+++ backend/app/schemas/dia_schema.py
@@ -1,13 +1,21 @@
+from datetime import date
+
+from app.schemas.bloque_schema import BloqueRead
 from sqlmodel import SQLModel
-from app.models.dia import Dia, DiaBase, Estado
+
+from app.models.dia import DiaBase, Estado
 
 
 class DiaCreate(DiaBase):
   pass
 
 
-class DiaRead(Dia):
-  pass
+class DiaRead(DiaBase):
+  fecha: date
+  bloques: list[BloqueRead] = []
+
+  class Config:
+    orm_mode = True
 
 
 class DiaUpdate(SQLModel):
