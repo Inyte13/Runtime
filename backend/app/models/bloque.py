ac5671a2b37cdeb7f1c2e00760fc6141a26180a8
Inyte13 <luisgardaz13@gmail.com>
2026-01-06 16:33:55 -0500

    actividad en table=True

--- backend/app/models/bloque.py
+++ backend/app/models/bloque.py
@@ -14,11 +14,11 @@ class BloqueBase(SQLModel):
   descripcion: str | None = Field(default=None, max_length=255)
   # Foreign key, relación 1-1
   id_actividad: int = Field(foreign_key="actividad.id", nullable=False)
-  actividad: Actividad | None = Relationship()
 
 
 class Bloque(BloqueBase, table=True):
   id: int | None = Field(default=None, primary_key=True)
   # Foreign key, heredamos la fecha de día
-  fecha: date = Field(foreign_key="dia.fecha")
-  dia: Dia = Relationship(back_populates="bloques")
+  fecha: date = Field(foreign_key="dia.fecha", nullable=False)
+  dia: "Dia" = Relationship(back_populates="bloques")
+  actividad: Actividad | None = Relationship()
