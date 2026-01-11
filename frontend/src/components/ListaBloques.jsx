0bacc480ed7bd9e067e73fcf326a1a35264ae544
Inyte13 <luisgardaz13@gmail.com>
2026-01-10 23:03:40 -0500

    anadir un bloque

--- frontend/src/components/ListaBloques.jsx
+++ frontend/src/components/ListaBloques.jsx
@@ -1,7 +1,25 @@
+import useBloques from '../hooks/useBloques'
 import BloqueCard from './BloqueCard'
 import styles from './ListaBloques.module.css'
 
-export default function ListaBloques ({ bloques, loading }) {
+export default function ListaBloques ({ fecha }) {
+  const { bloques, loading, fetchBloques, eliminarBloque } = useBloques(fecha)
+  const manejarAnadir = async () => {
+    const nuevoBloque = {
+      id_actividad: 13,
+      descripcion: '',
+      fecha: fecha.toLocaleDateString('sv-SE'),
+      hora: '23:30'
+    }
+    const response = await fetch('/bloques', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(nuevoBloque)
+    })
+    if (response.ok) {
+      fetchBloques()
+    }
+  }
   return (
     <>
       <div className={styles.listaBloques}>
@@ -12,9 +30,13 @@ export default function ListaBloques ({ bloques, loading }) {
           <p>Este día no tiene ningún bloque</p>
         )}
         {bloques.map(bloque => (
-          <BloqueCard key={bloque.id} bloque={bloque} />
+          <BloqueCard
+            key={bloque.id}
+            bloque={bloque}
+            manejarEliminacion={() => eliminarBloque(bloque.id)}
+          />
         ))}
-        <button className='btn'>Añadir</button>
+        <button className='btn' onClick={manejarAnadir}>Añadir</button>
       </div>
     </>
   )
