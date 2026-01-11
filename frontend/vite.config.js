f26890eeccac201a46df5fe2b0bc51bfd0bef696
Inyte13 <luisgardaz13@gmail.com>
2026-01-10 23:04:24 -0500

    rutas simplificado

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/bloques': 'http://127.0.0.1:8000',
+      '/dias': 'http://127.0.0.1:8000',
+      '/configuracion': 'http://127.0.0.1:8000',
+      '/actividades': 'http://127.0.0.1:8000'
+    }
+  }
 })
