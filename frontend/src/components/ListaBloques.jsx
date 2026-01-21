c279bf850183799b69cac7f91ea09719d82f92fe
Inyte13 <luisgardaz13@gmail.com>
2026-01-21 09:12:41 -0500

    componetizar

--- frontend/src/components/ListaBloques.jsx
+++ frontend/src/components/ListaBloques.jsx
@@ -1,55 +1,28 @@
-import useBloques from '../hooks/useBloques'
+import { useEffect } from 'react'
+import { useBloquesStore } from '../store/bloquesStore.js'
+import { useFechaStore } from '../store/fechaStore.js'
 import Card from './Card.jsx'
 import styles from './ListaBloques.module.css'
 
-export default function ListaBloques ({ fecha }) {
-  const { bloques, loading, fetchBloques, eliminarBloque } = useBloques(fecha)
-  const manejarAnadir = async () => {
-    let nuevoBloque
-
-    if (bloques.length === 0) {
-      nuevoBloque = {
-        id_actividad: 1,
-        descripcion: '',
-        fecha: fecha.toLocaleDateString('sv-SE'),
-        hora: '00:00'
-      }
-    } else {
-      const anterior = bloques[bloques.length - 1]
-      nuevoBloque = {
-        id_actividad: 2,
-        descripcion: '',
-        fecha: fecha.toLocaleDateString('sv-SE'),
-        hora: anterior.hora_fin
-      }
-    }
-    const response = await fetch('/bloques', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(nuevoBloque)
-    })
-    if (response.ok) {
-      fetchBloques()
-    }
-  }
+export default function ListaBloques () {
+  const { bloques, loading, crearBloque, traerBloques } = useBloquesStore()
+  const { fecha } = useFechaStore()
+  useEffect(() => {
+    traerBloques()
+  }, [fecha])
 
   return (
     <>
       <div className={styles.listaBloques}>
-        {loading && (
-          <p>Cargando...</p>
-        )}
-        {bloques.length === 0 && (
-          <p>Este día no tiene ningún bloque</p>
-        )}
+        {loading && <p>Cargando...</p>}
+        {!loading && bloques.length === 0 && <p>Este día no tiene ningún bloque</p>}
         {bloques.map(bloque => (
           <Card
             key={bloque.id}
             bloque={bloque}
-            manejarEliminacion={() => eliminarBloque(bloque.id)}
           />
         ))}
-        <button className='btn' onClick={manejarAnadir}>
+        <button className='btn' onClick={crearBloque}>
           <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' className='icon icon-tabler icons-tabler-outline icon-tabler-plus'><path stroke='none' d='M0 0h24v24H0z' fill='none' /><path d='M12 5l0 14' /><path d='M5 12l14 0' /></svg>
         </button>
       </div>
