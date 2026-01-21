692a7d799fba7374823ef134ea612926144f4764
Inyte13 <luisgardaz13@gmail.com>
2026-01-21 09:12:16 -0500

    colocPicker

---
+++ frontend/src/components/ColorPicker.jsx
@@ -0,0 +1,20 @@
+import { useActividadesStore } from '../store/actividadesStore'
+import './ColorPicker.module.css'
+
+export default function ColorPicker ({ bloque, color, setColor }) {
+  const { actualizarActividad } = useActividadesStore()
+  const manejarCambioColor = async (e) => {
+    await actualizarActividad(bloque.actividad.id, { color: e.target.value })
+  }
+  return (
+    <input
+      type='color'
+      value={color}
+      style={{ background: color }}
+      // onChange solo para cambiar en el frontend
+      onChange={e => setColor(e.target.value)}
+      // onBlur se ejecuta cuando cambia el foco
+      onBlur={manejarCambioColor}
+    />
+  )
+}
