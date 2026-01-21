38417e318c7cb0323a1ce37b2ca7dc78e034de46
Inyte13 <luisgardaz13@gmail.com>
2026-01-21 09:12:00 -0500

    componetizar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,32 +1,14 @@
 import CalendarioHeader from '../components/CalendarioHeader'
 import Details from '../components/Details'
 import styles from './Home.module.css'
-import { useState } from 'react'
 
 export default function Home () {
-  const [fecha, setFecha] = useState(new Date())
-  const prevDia = () => {
-    setFecha(d => new Date(d.getFullYear(), d.getMonth(), d.getDate() - 1))
-  }
-  const nextDia = () => {
-    setFecha(d => new Date(d.getFullYear(), d.getMonth(), d.getDate() + 1))
-  }
-  const irHoy = () => {
-    setFecha(new Date())
-  }
   return (
     <>
       <div className={styles.calendarioWrapper}>
-        <CalendarioHeader
-          fecha={fecha}
-          prevDia={prevDia}
-          nextDia={nextDia}
-          irHoy={irHoy}
-        />
+        <CalendarioHeader />
       </div>
-      <Details
-        fecha={fecha}
-      />
+      <Details />
     </>
   )
 }
