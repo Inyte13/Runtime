8b9686700138296e5a54fcf26872d6dd7485500f
Inyte13 <luisgardaz13@gmail.com>
2026-02-03 21:59:57 -0500

    echo

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -4,7 +4,7 @@ from fastapi import Depends
 from sqlmodel import Session, SQLModel, create_engine
 
 DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/runtime_db"
-
+# echo, para ver las lineas sql
 engine = create_engine(DATABASE_URL, echo=True)
 
 
