9b3954dfe6e520159a42435092fa38c6218a6a5f
Inyte13 <luisgardaz13@gmail.com>
2026-02-12 12:25:20 -0500

    cambiar testing integration por sqlite

--- backend/app/tests/integration/test_actividad_services.py
+++ backend/app/tests/integration/test_actividad_services.py
@@ -10,57 +10,58 @@ from app.services.actividad_service import (
 )
 
 
-def test_registrar_actividad_valida(session_mysql):
-  actividad = registrar_actividad(session_mysql, ActividadCreate(nombre="Actividad 1"))
+def test_registrar_actividad_valida(session_sqlite):
+  actividad = registrar_actividad(session_sqlite, ActividadCreate(nombre="Actividad 1"))
   assert actividad.id is not None
   assert actividad.nombre == "actividad 1"
 
 
-def test_registrar_actividad_duplicada(session_mysql):
-  registrar_actividad(session_mysql, ActividadCreate(nombre="Actividad 1"))
+def test_registrar_actividad_duplicada(session_sqlite):
+  registrar_actividad(session_sqlite, ActividadCreate(nombre="Actividad 1"))
   with pytest.raises(HTTPException) as exc_info:
-    registrar_actividad(session_mysql, ActividadCreate(nombre="actividad 1"))
+    registrar_actividad(session_sqlite, ActividadCreate(nombre="actividad 1"))
   assert exc_info.value.status_code == 400
   assert exc_info.value.detail == "Ya existe una actividad con ese nombre"
 
 
-def test_buscar_actividad_valida(session_mysql):
-  actividad = registrar_actividad(session_mysql, ActividadCreate(nombre="Actividad 1"))
+def test_buscar_actividad_valida(session_sqlite):
+  actividad = registrar_actividad(session_sqlite, ActividadCreate(nombre="Actividad 1"))
   assert actividad.id is not None
-  actividad_bd = buscar_actividad(session_mysql, actividad.id)
+  actividad_bd = buscar_actividad(session_sqlite, actividad.id)
   assert actividad_bd.id == actividad.id
   assert actividad_bd.nombre == "actividad 1"
 
 
-
-def test_buscar_actividad_invalida(session_mysql):
+def test_buscar_actividad_invalida(session_sqlite):
   with pytest.raises(HTTPException) as exc_info:
-    buscar_actividad(session_mysql, 9999)
+    buscar_actividad(session_sqlite, 9999)
   assert exc_info.value.status_code == 404
   assert exc_info.value.detail == "Actividad no encontrada"
 
 
-def test_actualizar_actividad_nombre_igual(session_mysql):
-  actividad = registrar_actividad(session_mysql, ActividadCreate(nombre="Actividad 1"))
+def test_actualizar_actividad_nombre_igual(session_sqlite):
+  actividad = registrar_actividad(session_sqlite, ActividadCreate(nombre="Actividad 1"))
   actualizado = actualizar_actividad(
-    session_mysql, actividad.id, ActividadUpdate(nombre="actividad 1")
+    session_sqlite, actividad.id, ActividadUpdate(nombre="actividad 1")
   )
   assert actualizado.nombre == "actividad 1"
 
 
-def test_actualizar_actividad_nombre_duplicado(session_mysql):
-  registrar_actividad(session_mysql, ActividadCreate(nombre="Actividad 1"))
-  actividad2 = registrar_actividad(session_mysql, ActividadCreate(nombre="Actividad 2"))
+def test_actualizar_actividad_nombre_duplicado(session_sqlite):
+  registrar_actividad(session_sqlite, ActividadCreate(nombre="Actividad 1"))
+  actividad2 = registrar_actividad(
+    session_sqlite, ActividadCreate(nombre="Actividad 2")
+  )
   with pytest.raises(HTTPException) as exc_info:
     actualizar_actividad(
-      session_mysql, actividad2.id, ActividadUpdate(nombre="actividad 1")
+      session_sqlite, actividad2.id, ActividadUpdate(nombre="actividad 1")
     )
   assert exc_info.value.status_code == 400
   assert exc_info.value.detail == "Ya existe una actividad con ese nombre"
 
 
-def test_eliminar_actividad_soft(session_mysql):
-  actividad = registrar_actividad(session_mysql, ActividadCreate(nombre='Actividad 1'))
-  eliminar_actividad_soft(session_mysql, actividad.id)
-  actividad_bd = buscar_actividad(session_mysql, actividad.id)
+def test_eliminar_actividad_soft(session_sqlite):
+  actividad = registrar_actividad(session_sqlite, ActividadCreate(nombre="Actividad 1"))
+  eliminar_actividad_soft(session_sqlite, actividad.id)
+  actividad_bd = buscar_actividad(session_sqlite, actividad.id)
   assert actividad_bd.is_active is False
