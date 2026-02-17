9a5e2b32f242e36b5c52aa373f341c8f4461d42a
Inyte13 <luisgardaz13@gmail.com>
2026-02-16 22:37:53 -0500

    para tailwind

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,11 @@
+export default {
+  darkMode: 'class',
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './node_modules/shadcn-ui/dist/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+}
