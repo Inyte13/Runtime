07847d627d7b6d9c807afe124e52a0548686bd27
Inyte13 <luisgardaz13@gmail.com>
2026-02-23 23:42:28 -0500

    main centrado

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,13 +8,13 @@ const Home = lazy(() => import('./pages/Home.js'))
   // El overflow-hidden en el main es para proteger al header
 export default function App() {
   return (
-    <>
+    <div className='flex flex-col h-dvh overflow-hidden'>
       <Header />
-      <main>
+      <main className='flex-1 overflow-hidden p-4 max-w-6xl mx-auto w-full'>
         <Routes>
           <Route path='/' element={<Home />} />
         </Routes>
       </main>
-    </>
+    </div>
   )
 }
