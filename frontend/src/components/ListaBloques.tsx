9fd01f0fab7db910188cd70c6c54e33b538a8198
Inyte13 <luisgardaz13@gmail.com>
2026-02-23 23:52:02 -0500

    semantica y icons lucide

--- frontend/src/components/ListaBloques.tsx
+++ frontend/src/components/ListaBloques.tsx
@@ -9,28 +9,19 @@ export default function ListaBloques({ bloques }: {bloques: BloqueRead[]}) {
   const crearBloque = useDiasStore(state => state.crearBloque)
 
   return (
-    <div className={styles.listaBloques}>
-      {bloques.map(bloque => (
-        <Card key={bloque.id} bloque={bloque} />
-      ))}
-      <button className='btn' onClick={crearBloque}>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          width='24'
-          height='24'
-          viewBox='0 0 24 24'
-          fill='none'
-          stroke='currentColor'
-          strokeWidth='2'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-          className='icon icon-tabler icons-tabler-outline icon-tabler-plus'
-        >
-          <path stroke='none' d='M0 0h24v24H0z' fill='none' />
-          <path d='M12 5l0 14' />
-          <path d='M5 12l14 0' />
-        </svg>
-      </button>
-    </div>
+    <section className='flex flex-col h-full' >
+      <ScrollArea className='flex-1 min-h-0 rounded-md border-none px-4'>
+        <ul className='flex flex-col gap-y-3'>
+          {bloques.map(bloque => (
+            <li key={bloque.id}>
+              <Bloque bloque={bloque} />
+            </li>
+          ))}
+        </ul>
+        <Button className='w-full mt-3'onClick={crearBloque}>
+          <Plus />
+        </Button>
+      </ScrollArea>
+    </section>
   )
 }
