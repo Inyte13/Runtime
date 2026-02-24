344d651192d7cda4b74a159a051049d98cf3d3f3
Inyte13 <luisgardaz13@gmail.com>
2026-02-23 23:43:49 -0500

    Nombre cambiado de Card -> Bloque

--- frontend/src/components/Bloque.tsx
+++ frontend/src/components/Bloque.tsx
@@ -1,32 +1,31 @@
-import styles from './Card.module.css'
-import CardHeader from './CardHeader'
 import { useDiasStore } from '../store/diasStore'
 import { BloqueRead } from '../types/Bloque'
 import { useColorStore } from '../store/colorStore'
+import BloqueHeader from './BloqueHeader'
+import { Input } from './ui/input'
 
-export default function Card({ bloque }: { bloque: BloqueRead }) {
+export default function Bloque({ bloque }: { bloque: BloqueRead }) {
   const actualizarBloque = useDiasStore(state => state.actualizarBloque)
   const color = useColorStore(
     state => state.colores[bloque.actividad.id] || bloque.actividad.color
   )
-  const manejarDescripcion = async (
-    e: React.FocusEvent<HTMLTextAreaElement>
-  ) => {
+  const manejarDescripcion = async (e: React.FocusEvent<HTMLInputElement>) => {
     await actualizarBloque(bloque.id, { descripcion: e.target.value })
   }
 
   return (
     <article
-      className={styles.bloque}
-      style={{ '--color-bloque': color } as React.CSSProperties}
+      className='border border-border border-l-2 rounded-md px-4 py-2 relative bg-card flex flex-col gap-0.5'
+      style={{ borderLeftColor: `${color}95` }}
     >
-      <CardHeader bloque={bloque} />
+      <BloqueHeader bloque={bloque} />
       <div>
         <span>
           {bloque.hora} - {bloque.hora_fin}
         </span>
       </div>
-      <textarea
+      <Input
+        className='focus:border-b focus:border-input italic'
         defaultValue={bloque.descripcion || ''}
         placeholder='Añadir descripción'
         onBlur={manejarDescripcion}
