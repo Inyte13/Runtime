57b385a2f2751dd436b5ceafa66c2e1f0a86d0cb
Inyte13 <luisgardaz13@gmail.com>
2026-02-23 23:51:43 -0500

    selector casero reemplazado

--- frontend/src/components/SelectorActividad.tsx
+++ frontend/src/components/SelectorActividad.tsx
@@ -29,40 +29,5 @@ export default function SelectorActividad({ bloque }: { bloque: BloqueRead }) {
 
 
   return (
-    <div ref={modalRef}>
-      <button onClick={manejarClick}>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          width='24'
-          height='24'
-          viewBox='0 0 24 24'
-          fill='none'
-          stroke='currentColor'
-          strokeWidth='2'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-          className='icon icon-tabler icons-tabler-outline icon-tabler-menu-2'
-        >
-          <path stroke='none' d='M0 0h24v24H0z' fill='none' />
-          <path d='M4 6l16 0' />
-          <path d='M4 12l16 0' />
-          <path d='M4 18l16 0' />
-        </svg>
-        {selector && (
-          <div>
-            <ul>
-              {actividades.map(act => (
-                <li key={act.id} onClick={() => manejarSelector(act)}>
-                  <span style={{ background: act.color }} />
-                  <a>
-                    {act.nombre.charAt(0).toUpperCase() + act.nombre.slice(1)}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-      </button>
-    </div>
   )
 }
