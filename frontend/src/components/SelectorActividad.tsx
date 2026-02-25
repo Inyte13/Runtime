d7a4fa0192b7cdeddb64f7ebba6120eca7eed50f
Inyte13 <luisgardaz13@gmail.com>
2026-02-24 21:45:01 -0500

    selector al fin

--- frontend/src/components/SelectorActividad.tsx
+++ frontend/src/components/SelectorActividad.tsx
@@ -11,9 +11,7 @@ import {
   ComboboxList,
   ComboboxTrigger,
   ComboboxValue,
-  useComboboxAnchor,
 } from './ui/combobox.js'
-import { ScrollArea } from './ui/scroll-area.js'
 import { Button } from './ui/button.js'
 import { ChevronDown } from 'lucide-react'
 
@@ -23,11 +21,75 @@ export default function SelectorActividad({ bloque }: { bloque: BloqueRead }) {
   const traerActividades = useActividadesStore(state => state.traerActividades)
   const setColor = useColorStore(state => state.setColor)
 
+  const manejarSelector = async (id: string | null) => {
+    if (!id) return
+    const actividad = actividades.find(
+      actividad => actividad.id.toString() === id
+    )
+    if (!actividad) return
     await actualizarBloque(bloque.id, { id_actividad: actividad.id })
     setColor(actividad.id, actividad.color)
   }
 
+  const items = actividades.map(act => ({
+    label: act.nombre,
+    value: act.id.toString(),
+    color: act.color,
+  }))
 
   return (
+    <Combobox
+      items={items}
+      onValueChange={manejarSelector}
+      value={bloque.actividad?.id.toString()}
+      onOpenChange={isOpen => {
+        if (isOpen && actividades.length === 0) traerActividades()
+      }}
+    >
+      <ComboboxTrigger
+        render={
+          <Button variant='ghost' className='p-1! text-2xl font-light gap-1'>
+            <div className='flex items-center gap-2'>
+              {bloque.actividad && (
+                <span
+                  className='size-4 rounded-full shrink-0'
+                  style={{ backgroundColor: bloque.actividad.color }}
+                />
+              )}
+              <span className='capitalize'>
+                <ComboboxValue />
+              </span>
+            </div>
+            <ChevronDown className='size-4 opacity-50' />
+          </Button>
+        }
+      />
+
+      <ComboboxContent className='w-[17ch] text-popover-foreground/90'>
+        <ComboboxInput
+          showTrigger={false}
+          showClear={true}
+          placeholder='Buscar'
+        />
+
+        <ComboboxEmpty className='py-4 text-center text-sm text-muted-foreground'>
+          No se encontraron actividades
+        </ComboboxEmpty>
+
+        <ComboboxList className='[&::-webkit-scrollbar]:hidden [scrollbar-width:none] '>
+          {item => (
+            <ComboboxItem key={item.value} value={item.value} className='py-1'>
+              <div className='flex items-center gap-2'>
+                <span
+                  className='size-3 rounded-full shrink-0'
+                  style={{ backgroundColor: item.color }}
+                />
+                <span className='truncate capitalize'>{item.label}</span>
+              </div>
+            </ComboboxItem>
+          )}
+        </ComboboxList>
+      </ComboboxContent>
+    </Combobox>
   )
 }
