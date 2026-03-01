0e8ac07eafa30ab510f36aec1a3f81b0167596bd
Inyte13 <luisgardaz13@gmail.com>
2026-03-01 11:37:51 -0500

    chore: llevado a controles duracion

--- frontend/src/components/BloqueHeader.tsx
+++ frontend/src/components/BloqueHeader.tsx
@@ -6,7 +6,6 @@ import { Button } from './ui/button'
 import { memo } from 'react'
 import ControlesDuracion from './ControlesDuracion'
 
-  const actualizarBloque = useDiasStore(state => state.actualizarBloque)
 export default memo(function BloqueHeader({
   bloque,
   duracion,
@@ -17,40 +16,13 @@ export default memo(function BloqueHeader({
   manejarDuracion: (newDuracion: number) => void
 }) {
   const eliminarBloque = useDiasStore(state => state.eliminarBloque)
-  const [duracion, setDuracion] = useState(bloque.duracion || 0)
-
-  const manejarDuracion = async (newDuracion: number) => {
-    setDuracion(newDuracion)
-    await actualizarBloque(bloque.id, { duracion: newDuracion })
-  }
-  const nextTime = () => {
-    const newDuracion = duracion + 0.5
-    manejarDuracion(newDuracion)
-  }
-  const prevTime = () => {
-    const newDuracion = Math.max(0, duracion - 0.5)
-    manejarDuracion(newDuracion)
-  }
   return (
     <header className='flex justify-between'>
-        <SelectorActividad bloque={bloque} />
-      <div className='flex mr-5 justify-center items-center'>
-        <span className='text-3xl font-extralight'>{duracion || '0'}h</span>
-        <div className='flex flex-col '>
-          <Button
-            size='icon-xs'
-            onClick={prevTime}
-            disabled={duracion === 0}
-            className={`${duracion === 0 ? '' : ''}`}
-            variant='ghost'
-          >
-            <ChevronUp />
-          </Button>
-          <Button onClick={nextTime} size='icon-xs' variant='ghost'>
-            <ChevronDown />
-          </Button>
-        </div>
-      </div>
+      <SelectorActividad bloque={bloque} />
+      <ControlesDuracion
+        duracion={duracion}
+        manejarDuracion={manejarDuracion}
+      />
       <Button
         size='icon-xxs'
         variant='destructive'
