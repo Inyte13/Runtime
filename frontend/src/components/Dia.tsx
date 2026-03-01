eb0795b9a7070bc4d617ca5e3f4bb16c69da0e8e
Inyte13 <luisgardaz13@gmail.com>
2026-03-01 11:37:11 -0500

    chore: llevado al diaHeader

--- frontend/src/components/Dia.tsx
+++ frontend/src/components/Dia.tsx
@@ -6,34 +6,14 @@ import DiaHeader from './DiaHeader'
 
 export default function Dia() {
   const fecha = useFechaStore(state => state.fecha)
-  const fechaISO = formatFechaISO(fecha)
-  const fechaDetail = formatFechaDetail(fecha)
-  
-  const diaDetail = useDiasStore(state => state.diaDetail)
-  const bloques = diaDetail?.bloques || []
-
   const traerDiaDetail = useDiasStore(state => state.traerDiaDetail)
-  const actualizarDia = useDiasStore(state => state.actualizarDia)
-  
   useEffect(() => {
     traerDiaDetail()
   }, [fecha, traerDiaDetail])
 
-  const manejarTitulo = async (e: React.FocusEvent<HTMLInputElement>) => {
-    await actualizarDia(fechaISO, { titulo: e.target.value })
-  }
   return (
-    <section className='flex flex-col min-w-90 h-full gap-y-2 p-4' >
-      <header className='flex flex-col gap-y-1 items-center'>
-        <h2 className='text-2xl font-semibold'>{fechaDetail}</h2>
-        <Input
-          className='text-center text-base italic border-0'
-          defaultValue={diaDetail?.titulo || ''}
-          placeholder='Añadir título'
-          onBlur={manejarTitulo}
-          maxLength={150}
-        />
-      </header>
+    <section className='flex flex-col min-w-90 h-full gap-y-2 p-4'>
+      <DiaHeader />
       <div className='flex-1 min-h-0'>
         <ListaBloques bloques={bloques} />
       </div>
