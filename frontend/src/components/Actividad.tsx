a75092792f1b0608f5506358f73237d441ff1da3
Inyte13 <luisgardaz13@gmail.com>
2026-03-02 23:15:41 -0500

    chore: capitalize igual que en el selector

--- frontend/src/components/Actividad.tsx
+++ frontend/src/components/Actividad.tsx
@@ -2,14 +2,13 @@ import { memo } from 'react'
 import ColorPicker from './ColorPicker'
 import { useActividadesStore } from '../store/actividadesStore'
 
-  const nombreBd = actividad.nombre
-  const nombre = nombreBd.charAt(0).toUpperCase() + nombreBd.slice(1)
 // TODO: El primer cambio si renderiza todos los
 export default memo(function Actividad({ id }: { id: number }) {
   return (
     <div className='flex items-center gap-2 p-1.5 hover:bg-accent hover:text-accent-foreground cursor-pointer rounded-lg'>
       <ColorPicker actividad={actividad} />
       <span>{nombre}</span>
+      <span className='capitalize'>{nombre}</span>
     </div>
   )
 })
