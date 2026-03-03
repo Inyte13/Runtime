c3127a49bfcc2208c76097d21e8d1f6cae20f97a
Inyte13 <luisgardaz13@gmail.com>
2026-03-02 23:45:02 -0500

    chore: solo si es el ultimo se puede manejar la duracion

--- frontend/src/components/ControlesDuracion.tsx
+++ frontend/src/components/ControlesDuracion.tsx
@@ -5,12 +5,14 @@ import { memo } from 'react'
 export default memo(function ControlesDuracion({
   duracion,
   manejarDuracion,
+  isLast,
 }: {
   duracion: number
   manejarDuracion: (newDuracion: number) => void
+  isLast: boolean
 }) {
-  const nextTime = () => manejarDuracion(duracion + 0.5)
-  const prevTime = () => manejarDuracion(Math.max(0, duracion - 0.5))
+  const nextTime = () => isLast && manejarDuracion(duracion + 0.5)
+  const prevTime = () => isLast && manejarDuracion(Math.max(0, duracion - 0.5))
   return (
     <div className='flex mr-5 justify-center items-center'>
       <span className='text-3xl font-extralight'>{duracion}h</span>
@@ -18,12 +20,17 @@ export default memo(function ControlesDuracion({
         <Button
           size='icon-xs'
           onClick={prevTime}
-          disabled={duracion === 0}
+          disabled={!isLast || duracion === 0}
           variant='ghost'
         >
           <ChevronUp />
         </Button>
-        <Button onClick={nextTime} size='icon-xs' variant='ghost'>
+        <Button
+          onClick={nextTime}
+          size='icon-xs'
+          variant='ghost'
+          disabled={!isLast}
+        >
           <ChevronDown />
         </Button>
       </div>
