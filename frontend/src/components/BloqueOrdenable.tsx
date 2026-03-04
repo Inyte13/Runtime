c3904070347f1a9ee701a94db60f9dd280ae07f5
Inyte13 <luisgardaz13@gmail.com>
2026-03-03 21:24:52 -0500

    chore: pasamos el grip a bloque

--- frontend/src/components/BloqueOrdenable.tsx
+++ frontend/src/components/BloqueOrdenable.tsx
@@ -20,15 +20,7 @@ export default memo(function BloqueOrdenable({ id }: { id: number }) {
       style={style}
       className='list-none w-full relative'
     >
-      <Bloque id={id} />
-      <div
-        // El handle, el unico receptor del agarre
-        {...attributes}
-        {...listeners}
-        className='absolute top-1/2 -translate-y-1/2 right-0 z-10 cursor-grab active:cursor-grabbing opacity-30 hover:opacity-100 transition-opacity'
-      >
-        <GripVertical />
-      </div>
+      <Bloque id={id} attributes={attributes} listeners={listeners} />
     </li>
   )
 })
